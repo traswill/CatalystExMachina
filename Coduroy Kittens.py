0f5bc6c805b20e1c40f2d25ee225c549a89248e3
traswill <travisw@email.sc.edu>
2018-01-20 22:21:28 -0500

    First Commit!  Call me a hero.

--- Coduroy Kittens.py
+++ Coduroy Kittens.py
@@ -3,8 +3,3 @@
 from sklearn.ensemble import RandomForestRegressor
 
 
-# Feature List
-feature_names = ['Metal', 'Metal Loading', 'Support',]
-
-# use metal to lookup bajillion properties
-# maybe same with the support (or input them)
