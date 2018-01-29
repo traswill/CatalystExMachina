f262679f8946cd66ca5f074fcb96c5da1f1900de
Travis <travisw@email.sc.edu>
2018-01-29 11:33:55 -0500

    Still rearranging my catalyst stuff

--- Coduroy Kittens.py
+++ Coduroy Kittens.py
@@ -2,4 +2,15 @@
 
 from sklearn.ensemble import RandomForestRegressor
 
+class catalyst():
+    def __init__(self):
+        self.labels = None
+        self.features = None
+
+    def set(self, labels=None, features=None):
+        self.labels = labels
+        self.features = features
+
+
+
 
