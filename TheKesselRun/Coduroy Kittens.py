937d14c4934405bfc8e7ece3e251c6dc0236c212
Travis <travisw@email.sc.edu>
2018-02-28 14:05:17 -0500

    Converted program to use dictionaries. Should make addition of new features streamlined.

--- TheKesselRun/Coduroy Kittens.py
+++ TheKesselRun/Coduroy Kittens.py
@@ -26,6 +26,15 @@ class Learner():
         self.learner = None
 
     def add_catalyst(self, index, catalyst):
+        base_index = index
+        mod = 0
+
+        # Determine if key already exists in dictionary, modify key if so
+        while index in self.catalyst_dictionary:
+            mod += 1
+            index = '{}_{}'.format(base_index, mod)
+
+        # Add to dictionary
         self.catalyst_dictionary[index] = catalyst
 
     def get_training_data_size(self):
@@ -137,18 +146,38 @@ class Learner():
 
         show(p)
 
+    def load_NH3_catalysts(self):
+        df = pd.read_excel(r".\Data\NH3Data.xlsx", index_col=0)
+
+        for vals in df.iterrows():
+            if np.isnan(vals[0]):
+                continue
+
+            for temp in [250, 300, 350, 400, 450]:
+                cat = Catalyst()
+                cat.ID = int(vals[0])
+                inputs = vals[1]
+
+                cat.add_element(inputs['Element_1'], inputs['Loading_1'])
+                cat.add_element(inputs['Element_2'], inputs['Loading_2'])
+                cat.add_element(inputs['Element_3'], inputs['Loading_3'])
+                cat.input_elements()
+
+                cat.input_reactor_number(inputs['Reactor'])
+                cat.input_temperature(temp)
+                cat.input_space_velocity('Space Velocity')
+                cat.input_ammonia_concentration('Ammonia Concentration')
+
+                cat.activity = inputs['T' + str(temp)]
+
+                self.add_catalyst(index='{ID}_{T}'.format(ID=cat.ID, T=temp), catalyst=cat)
+
+
 class Catalyst():
     """Catalyst will contain each individual training set"""
     def __init__(self):
         self.ID = None
-        self.label = None
-
-        self.features = None
-        self.feature_names = None
-        self.temperature = None
-
-        self.reactor_index = None
-        self.space_velocity = None
+        self.activity = None
 
         self.input_dict = dict()
         self.feature_dict = dict()
@@ -160,147 +189,59 @@ class Catalyst():
     def input_elements(self):
         self.input_dict['elements'] = self.elements
 
+    def add_element(self, element, weight_loading):
+        self.elements[element] = weight_loading
+
     def input_space_velocity(self, space_velocity):
         self.input_dict['space_velocity'] = space_velocity
 
     def input_reactor_number(self, reactor_number):
         self.input_dict['reactor_number'] = reactor_number
 
-    def add_element(self, element, weight_loading):
-        self.elements[element] = weight_loading
+    def input_ammonia_concentration(self, ammonia_concentration):
+        self.input_dict['ammonia_concentration'] = ammonia_concentration
 
-    def extract_element_features(self, ele):
-        """ Extract the features for a single material from Elements.csv"""
+    def feature_add(self, key, value):
+        self.feature_dict[key] = value
 
+    def feature_add_statistics(self, key, values):
+        # Maximum
+        self.feature_dict['{}_max'.format(key)] = np.max(values)
+
+        # Minimum
+        self.feature_dict['{}_min'.format(key)] = np.min(values)
+
+        # Mean
+        self.feature_dict['{}_mean'.format(key)] = np.mean(values)
+
+        # Median
+        self.feature_dict['{}_med'.format(key)] = np.median(values)
+
+    def feature_add_elemental_properties(self):
+        # Load Elements.csv as DataFrame
         eledf = pd.read_csv(r'./Data/ELements.csv', index_col=1)
-        feature_names = eledf.loc[str(ele)].index.values
-        features = eledf.loc[str(ele)].values
-        return features, feature_names
 
-    def create_element_feature_list(self, set=False):
-        """Create feature list using mean, median, etc of all elements in catalyst"""
+        # Slice Elements.csv based on elements present
+        eledf = eledf.loc[list(self.elements.keys())]
 
-        feature_df = None
+        for feature_name, feature_values in eledf.T.iterrows():
+            if feature_name == 'OxidationStates':
+                continue  # Oxidation State loads as a string and causes errors.  TODO: Fix this?
 
-        for ele in self.elements:
-            if ele is np.nan:
-                pass
-            else:
-                feat, feat_nm = self.extract_element_features(ele)
-                if feature_df is None:
-                    feature_df = pd.DataFrame(feat, columns=[ele], index=feat_nm)
-                else:
-                    temp_df = pd.DataFrame(feat, columns=[ele], index=feat_nm)
-                    feature_df = pd.concat([feature_df, temp_df], axis=1)
+            self.feature_add_statistics(feature_name, feature_values.values)
 
-        # Extract Statistic Properties from Elements.csv values
-        def extract(df, property):
-            # Extract max, mean, median, min for all proprties
-            return [
-                df.loc[property, :].max(),
-                df.loc[property, :].mean(),
-                df.loc[property, :].median(),
-                df.loc[property, :].min(),
-            ]
+    def feature_add_n_elements(self):
+        self.feature_dict['n_elements'] = len(self.elements.keys())
 
-        # Stoichiometric Features
-        stoich_features_dict = {
-            'n_ele': [len(self.elements)],
-        }
 
-        stoich_feature_list = list()
-        stoich_feature_nm_list = list()
-        for feat in stoich_features_dict:
-            stoich_feature_list += stoich_features_dict[feat]
-            stoich_feature_nm_list += [feat]
-
-        # Statistical Features (calculates max, mean, median, and min)
-        stat_features_dict = {
-            'atvol': extract(feature_df, 'AtomicVolume'),
-            'atwt': extract(feature_df, 'AtomicWeight'),
-            'atrad': extract(feature_df, 'CovalentRadius'),
-            'bT': extract(feature_df, 'BoilingTemp'),
-            'dens': extract(feature_df, 'Density'),
-            'eaff': extract(feature_df, 'ElectronAffinity'),
-            'eneg': extract(feature_df, 'Electronegativity'),
-            'mT': extract(feature_df, 'MeltingT'),
-            'f_unf': extract(feature_df, 'NfUnfilled'),
-            'f_val': extract(feature_df, 'NfValence'),
-            'd_unf': extract(feature_df, 'NdUnfilled'),
-            'd_val': extract(feature_df, 'NdValence'),
-            'p_unf': extract(feature_df, 'NpUnfilled'),
-            'p_val': extract(feature_df, 'NpValence'),
-            's_unf': extract(feature_df, 'NsUnfilled'),
-            's_val': extract(feature_df, 'NsValence'),
-            'n_unf': extract(feature_df, 'NUnfilled'),
-            'n_val': extract(feature_df, 'NValence')
-        }
-
-        stat_feature_list = list()
-        stat_feature_nm_list = list()
-        for feat in stat_features_dict:
-            stat_feature_list += stat_features_dict[feat]
-            stat_feature_nm_list += [
-                '{nm}_max'.format(nm=feat),
-                '{nm}_mean'.format(nm=feat),
-                '{nm}_median'.format(nm=feat),
-                '{nm}_min'.format(nm=feat)]
-
-        # Add all features and names to a list
-        features = list(self.elements.keys()) + list(self.elements.values())
-        feature_names = ['M1','M2','M3','W1','W2','W3']
-
-        features += [self.temperature] + [self.reactor_index] + [self.space_velocity]
-        feature_names += ['T','Reactor','Space Velocity']
-
-        features += stoich_feature_list
-        feature_names += stoich_feature_nm_list
-
-        features += stat_feature_list
-        feature_names += stat_feature_nm_list
-
-        if set:
-            self.features = features
-            self.feature_names = feature_names
-        else:
-
-            return features, feature_names
 
 
 if __name__ == '__main__':
+
     # Spool up Learner Class
     skynet = Learner()
-
-    # Create Catalysts
-    df = pd.read_excel(r".\Data\NH3Data.xlsx", index_col=0)
-
-    for vals in df.iterrows():
-        for temp in [250, 300, 350, 400, 450]:
-            cat = Catalyst()
-            cat.ID = vals[0]
-            params = vals[1]
-            cat.add_element(params['Metal 1'],params['M1 Wt %'])
-            cat.add_element(params['Metal 2'],params['M2 Wt %'])
-            cat.add_element(params['Metal 3'],params['M3 Wt %'])
-            cat.reactor_index = params['Reactor']
-            cat.temperature = temp
-            cat.label = params['T'+str(temp)]
-            cat.space_velocity = params['Space Velocity']
-            if np.isnan(cat.label):
-                continue
-            cat.create_element_feature_list(set=True)
-            if cat.complete():
-                # Prevent overwriting of duplicate keys
-                keymod = 0
-                catkey = '{ID}_{T}_{mod}'.format(ID=cat.ID, T=cat.temperature, mod=keymod)
-                while catkey in skynet.catalyst_dictionary:
-                    keymod+=1
-                    catkey = '{ID}_{T}_{mod}'.format(ID=cat.ID, T=cat.temperature, mod=keymod)
-
-                # Once catalyst has a unique ID modifier, add to skynet
-                skynet.add_catalyst(index=catkey, catalyst=cat)
-            else:
-                print('Catalyst {} NOT complete.  Not added to skynet.'.format(cat.ID))
+    skynet.load_NH3_catalysts()
+    exit()
 
     # Create training data
     skynet.create_training_set()
