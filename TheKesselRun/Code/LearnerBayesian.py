cf1d577685377273d833b29d1805cfcb5049fea7
Travis <travisw@email.sc.edu>
2018-12-07 17:15:57 -0500

    * Adding Bayesian methods

---
+++ TheKesselRun/Code/LearnerBayesian.py
@@ -0,0 +1,20 @@
+# Created by Travis Williams
+# Property of the University of South Carolina
+# Jochen Lauterbach Group
+# Contact: travisw@email.sc.edu
+# Code Start: December 7, 2018
+
+from TheKesselRun.Code.Plotter import Graphic
+from TheKesselRun.Code.Catalyst import CatalystObject, CatalystObservation
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import time
+
+class BayesianLearner():
+    def __init__(self):
+        pass
+
